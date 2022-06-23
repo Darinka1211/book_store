ed9f20223b09b77abd05315158d6fc1dcdec52fa
Darinka1211 <93863016+Darinka1211@users.noreply.github.com>
2022-06-23 17:43:27 +0300

    add new file-Button &Input

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
 
 import './App.scss';
+import { Button } from './components/Button';
 
 function App() {
   return (
     <div className="App">
      <h1>Hello</h1>
+     <Button className=""/>
     </div>
   );
 }
