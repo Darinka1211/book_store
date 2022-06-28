e9619f2333f0685f82729b4e3e138d8d1c2ac59e
Darinka1211 <93863016+Darinka1211@users.noreply.github.com>
2022-06-28 23:17:00 +0300

    add Input

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,26 @@
-import React from 'react';
+import React, {ReactElement} from 'react';
 
 import './App.scss';
-import { Button } from './components/Button';
-// import Cancelcross from "./components/Button/icon/Cancelcross.svg"
+import { Button } from "./components/Button/Button"
+import {Input} from "./components/Input/Input"
+
 
 function App() {
-  return (
+   return (
     <div className="App">
      <h1>Hello</h1>
      <Button className="button--add_to_card"  text="add to card" />
-     {/* <Button className="button--cross" icon={Cancelcross}/> */}
+     <Input title="Title" placeholder="Placeholder" />
+      <Input
+        type="email"
+        title="Title"
+        placeholder="Placeholder"
+        error={true}
+        
+        disabled
+      />
+      <Input type="password" title="Title" placeholder="Placeholder" />
+   
     </div>
   );
 }
--- src/components/Input/Input.tsx
+++ src/components/Input/Input.tsx
@@ -1,1 +1,34 @@
-import React from "react";
+import React from 'react';
+import './Input.scss';
+
+type InputProps = {
+    type?: React.HTMLInputTypeAttribute
+    title?: string
+    placeholder?: string
+    disabled?: boolean
+    error?: boolean
+    errorMessage?: string
+  }
+  
+  export const Input = ({
+    type,
+    title,
+    placeholder,
+    disabled,
+    error,
+    errorMessage,
+  }: InputProps) => {
+    return (
+      <div className="input">
+       
+        <input
+          id={type}
+          className={`input__input ${error ? 'input__input--error' : null}`}
+          type={type ?? 'text'}
+          placeholder={placeholder}
+          disabled={disabled}
+        />
+        <span className="input__error">{error && errorMessage}</span>
+      </div>
+    )
+  }
